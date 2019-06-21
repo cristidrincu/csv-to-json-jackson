df3c23c96d5a9612b927f97ceefe82f247866177
Cristian Drincu <cristiandrincu@Cristians-MacBook-Pro.local>
2019-06-21 14:47:22 +0300

    Create profile and address class

--- src/main/java/com/espresoh/entities/profiles/Address.java
+++ src/main/java/com/espresoh/entities/profiles/Address.java
@@ -1,4 +1,92 @@
 package com.espresoh.entities.profiles;
 
+import com.espresoh.entities.data.RawData;
+
 public class Address {
+
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String countryCode;
+    private String zipCode;
+    private String stateProvince;
+    private String addressType;
+
+    public Address(RawData rawData) {
+        this.addressLine1 = rawData.getAddress1();
+        this.addressLine2 = rawData.getAddress2();
+        this.countryCode = rawData.getCountryCode();
+        this.zipCode = rawData.getZipCode();
+        this.stateProvince = rawData.getStateProvince();
+        this.addressType = rawData.getAddressType();
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getStateProvince() {
+        return stateProvince;
+    }
+
+    public void setStateProvince(String stateProvince) {
+        this.stateProvince = stateProvince;
+    }
+
+    public String getAddressType() {
+        return addressType;
+    }
+
+    public void setAddressType(String addressType) {
+        this.addressType = addressType;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "addressLine1='" + addressLine1 + '\'' +
+                ", addressLine2='" + addressLine2 + '\'' +
+                ", city='" + city + '\'' +
+                ", countryCode='" + countryCode + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                ", stateProvince='" + stateProvince + '\'' +
+                ", addressType='" + addressType + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/espresoh/entities/profiles/Profile.java
+++ src/main/java/com/espresoh/entities/profiles/Profile.java
@@ -1,4 +1,151 @@
 package com.espresoh.entities.profiles;
 
+import com.espresoh.entities.data.RawData;
+import java.util.Date;
+
 public class Profile {
+
+     private int concilioId;
+     private String email;
+     private Date birthDate;
+     private String firstName;
+     private String lastName;
+     private String phone;
+     private String profilePhoto;
+     private String companyName;
+     private Date createdDate;
+     private Date lastModifiedDate;
+     private Date lastMergeDate;
+     private Date unmergeDate;
+     private Address address;
+
+    public Profile(RawData rawData) {
+        this.email = rawData.getEmail();
+        this.firstName = rawData.getFirstName();
+        this.lastName = rawData.getLastName();
+        this.address = new Address(rawData);
+    }
+
+    public int getConcilioId() {
+        return concilioId;
+    }
+
+    public void setConcilioId(int concilioId) {
+        this.concilioId = concilioId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public Date getLastMergeDate() {
+        return lastMergeDate;
+    }
+
+    public void setLastMergeDate(Date lastMergeDate) {
+        this.lastMergeDate = lastMergeDate;
+    }
+
+    public Date getUnmergeDate() {
+        return unmergeDate;
+    }
+
+    public void setUnmergeDate(Date unmergeDate) {
+        this.unmergeDate = unmergeDate;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Profile{" +
+                "concilioId=" + concilioId +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phone='" + phone + '\'' +
+                ", profilePhoto='" + profilePhoto + '\'' +
+                ", companyName='" + companyName + '\'' +
+                ", createdDate=" + createdDate +
+                ", lastModifiedDate=" + lastModifiedDate +
+                ", lastMergeDate=" + lastMergeDate +
+                ", unmergeDate=" + unmergeDate +
+                ", address=" + address +
+                '}';
+    }
 }
