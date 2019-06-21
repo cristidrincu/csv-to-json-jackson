e90124554036d1d4b6a1213a3991064575892179
Cristian Drincu <cristiandrincu@Cristians-MacBook-Pro.local>
2019-06-21 16:54:06 +0300

    Util for parsing and formatting dates

--- src/main/java/com/espresoh/commons/utils/DateFormatter.java
+++ src/main/java/com/espresoh/commons/utils/DateFormatter.java
@@ -1,4 +1,13 @@
 package com.espresoh.commons.utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateFormatter {
+
+    public static Date formatDate(String inDate, String dateFormat) throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat(dateFormat);
+        return formatter.parse(inDate);
+    }
 }
